d673cb26b4e3f43afa1fcd88292206b73d70cb97
vinu07 <vinuvpazhayannur@gmail.com>
2025-07-30 17:52:07 +0530

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css'
+import { WagmiProvider } from 'wagmi'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { config } from '../lib/web3Config'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
 }
 
 export default MyApp
