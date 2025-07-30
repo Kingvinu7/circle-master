62d87fab2196538e1597e53aeade2b1006c6226c
vinu07 <vinuvpazhayannur@gmail.com>
2025-07-30 09:39:00 +0530

    Update Next.js config for Termux compatibility

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,  // Disable SWC minification
+  swcMinify: false,  // Disable SWC completely for Termux
+  compiler: {
+    removeConsole: false,
+  },
+  experimental: {
+    esmExternals: false,
+  },
   images: {
     domains: [],
   },
 }
 
 module.exports = nextConfig
+
 
